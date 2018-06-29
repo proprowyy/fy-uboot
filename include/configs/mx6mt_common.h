31ae41ba7e48410b914c8e659dd91227acaf51bf
Your Name <you@example.com>
2018-06-29 11:53:55 +0800

    fix mmcargs

--- include/configs/mx6mt_common.h
+++ include/configs/mx6mt_common.h
@@ -227,7 +227,7 @@
 				"if test $board_name = SABRESD && test $board_rev = MX6Q; then " \
 					"setenv fdt_file imx6q-sabresd.dtb; fi; " \
 				"if test $board_name = SABRESD && test $board_rev = MX6DL; then " \
-					"setenv fdt_file imx6dl-sabresd.dtb; fi; " \
+					"setenv fdt_file imx6q-sabresd.dtb; fi; " \
 				"if test $fdt_file = undefined; then " \
 					"echo WARNING: Could not determine dtb to use; fi; " \
 			"fi;\0" \
